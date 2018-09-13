4068576b15a61e191fbc1073e4bb425ba6f827b8
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-12 20:50:30 -0400

    Mumuki Export on 2018-09-12 20:50:30 -0400

---
+++ 00003_Reforçando o que vimos/test..js
@@ -0,0 +1,7 @@
+describe("Executar imprimir5vezes5()", function() {
+  it("deveria imprimir 5 5 5 5 5", function() {
+	imprimir5vezes5();
+	assert.equal(console.toString(), "5\n5\n5\n5\n5\n")
+  });
+})
+
