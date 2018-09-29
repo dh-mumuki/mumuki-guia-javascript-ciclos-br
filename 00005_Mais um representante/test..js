44089e54151bf01b7799db477810c55a41bb7319
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 18:07:24 -0400

    Mumuki Export on 2018-09-29 18:07:24 -0400

--- 00005_Mais um representante/test..js
+++ 00005_Mais um representante/test..js
@@ -1,4 +1,4 @@
-describe("Imprimir passandoPor()", function() {
+describe("A função passandoPor()", function() {
   it("deve imprimir 'aqui eu tenho o valor de 0', 'aqui eu tenho o valor de 1', 'aqui eu tenho o valor de 2', 'aqui eu tenho o valor de 3'", function() {
 	passandoPor();
 	assert.equal(console.toString(), "aqui eu tenho o valor de 0 \naqui i tem o valor de 1 \naqui i tem o valor de 2 \naqui i tem o valor de 3")
