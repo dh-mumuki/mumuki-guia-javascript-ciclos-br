e1879bcb7abf29b8ed3b4a07396eac7ac716211f
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 18:24:45 -0400

    Mumuki Export on 2018-09-29 18:24:45 -0400

--- 00006_Casal ou não, eis a questão/test..js
+++ 00006_Casal ou não, eis a questão/test..js
@@ -1,6 +1,6 @@
 describe("Imprimir passandoPelosPares()", function() {
-  it("deve imprimir 'aqui i tem o valor de 0','aqui i tem o valor de 2','aqui i tem o valor de 4','aqui i tem o valor de 6'", function() {
+  it("deve imprimir 'aqui eu tenho o valor de 0','aqui eu tenho o valor de 2','aqui eu tenho o valor de 4','aqui eu tenho o valor de 6'", function() {
 	passandoPelosPares();
-	assert.equal(console.toString(), "aqui i tem o valor de 0\naqui i tem o valor de 2\naqui i tem o valor de 4\naqui i tem o valor de 6\n")
+	assert.equal(console.toString(), "aqui eu tenho o valor de 0\naqui eu tenho o valor de 2\naqui eu tenho o valor de 4\naqui eu tenho o valor de 6\n")
   });
 })
