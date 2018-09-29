7dad4b6a1fa50eca557e55b61567672718f0b7ee
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 19:11:33 -0400

    Mumuki Export on 2018-09-29 19:11:33 -0400

--- 00009_Muitas empanadas/test..js
+++ 00009_Muitas empanadas/test..js
@@ -5,6 +5,14 @@ describe("Calcular somarMoedasDe25(6)", function() {
 })
 describe("Calcular somarMoedasDe25(4)", function() {
   it("deveria devolver 1", function() {
-	assert.equal(somarMoedasDe25(4), 1)
+    (somarMoedasDe25(4)
+	assert.equal(console.toString(), 1)
+  });
+})
+
+describe("Calcular somar5MoedasDe25Centavos()", function() {
+  it("deveria devolver por tela 1.25", function() {
+    somar5MoedasDe25Centavos()
+	assert.equal(console.toString(), "1.25\n")
   });
 })
