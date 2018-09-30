043b5ee2ad98e64f03ece6fbd8939cb4a9a01e75
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 20:24:14 -0400

    Mumuki Export on 2018-09-29 20:24:14 -0400

--- 00012_Usain Bolt precisa de você!/test..js
+++ 00012_Usain Bolt precisa de você!/test..js
@@ -1,15 +1,15 @@
 describe("caloriasDeTrote(2)", function() {
-  it("Un trote de 2 vueltas deberia retornar 15", function() {
+  it("Um trote de 2 voltas deveria retornar 15", function() {
     assert.equal(caloriasDeTrote(2), 15)
   });
 })
 describe("caloriasDeTrote(3)", function() {
-  it("Un trote de 3 vueltas deberia retornar 30", function() {
+  it("Um trote de 2 voltas deveria retornar 30", function() {
     assert.equal(caloriasDeTrote(3), 30)
   });
 })
 describe("caloriasDeTrote(1)", function() {
-  it("Un trote de 1 vuelta deberia retornar 5", function() {
+  it("Um trote de 2 voltas deveria retornar 5", function() {
     assert.equal(caloriasDeTrote(1), 5)
   });
 })
