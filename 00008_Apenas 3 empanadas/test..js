8a65c09cf4ac21212f93bbd1bd9e8c13cf23f5de
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 20:10:34 -0400

    Mumuki Export on 2018-09-29 20:10:34 -0400

--- 00008_Apenas 3 empanadas/test..js
+++ 00008_Apenas 3 empanadas/test..js
@@ -1,6 +1,6 @@
 describe("Calcular somar5MoedasDe25Centavos()", function() {
   it("deveria devolver 1.25", function() {
-    somar5MoedasDe25Centavos()
-    assert.equal(console.toString(), "1.25\n")
+    
+    assert.equal(somar5MoedasDe25Centavos(), "1.25\n")
   });
 })
